3c21ad963c580194b12929444ab12776b15194f4
Natnat <n.aspeedelgado@gmail.com>
2023-11-14 18:53:58 -0300

    forecast with js loop

--- index.js
+++ index.js
@@ -57,3 +57,28 @@ function updateCity() {
 let searchForm = document.getElementById("searchForm");
 searchForm.addEventListener("submit", updateCity);
 searchCity("Valparaiso");
+
+function displayForecast(){
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml +=
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>
+    `;
+  });
+  let forecast = document.getElementById("forecast")
+  forecast.innerHTML = forecastHtml;
+}
+searchCity("Banff");
+displayForecast();
